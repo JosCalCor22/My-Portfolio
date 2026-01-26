77f227a5adc51a524a2ae872cdaa731fd47a6a15
JosCalCor22 <stevencalcor04@gmail.com>
2026-01-26 17:44:11 -0500

    Updating global files and resolve issue in vercel render

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/',
 })
